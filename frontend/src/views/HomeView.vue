70e9c0f6d4929cbfe35d8c18568d2edad08c79bc
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2025-02-18 14:59:20 +0200

    initial push, +Homepage

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HomepageComp/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomepageComp from '@/components/HomepageComp.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomepageComp
+  }
+}
+</script>
