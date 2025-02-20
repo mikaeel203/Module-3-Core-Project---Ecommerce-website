19fb5991e26d7aa72d29920f1f5a6785f98aaa15
mikaeel203 <sedickmikaeel@gmail.com>
2025-02-20 14:39:32 +0200

    setting up backend init

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 // import routes that we exported
 import userRoutes from './routes/userRoutes.js';
-import authenticateJWT from './middleware/authMiddleware.js'
 const PORT = process.env.PORT ||3000
 const app = express()
 app.use(cors({
