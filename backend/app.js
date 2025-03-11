0d9bde433e777fadf900227a44a2e6f5d7bbab9d
mikaeel203 <sedickmikaeel@gmail.com>
2025-03-11 13:46:48 +0200

    cors

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:8080', // Replace with your frontend URL
-    credentials: true,
+  
   }));
 app.use(express.json());
 
