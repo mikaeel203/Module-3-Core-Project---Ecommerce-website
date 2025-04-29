0a67d8183a203aee51273fd8d3efd7c6794ff6f4
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2025-04-30 01:16:21 +0200

    cloudinary db for images && updated product controller

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:8080', // Replace with your frontend URL
-    credentials: true,
+    // origin: 'http://localhost:8081', // Replace with your frontend URL
+    // credentials: true,
   }));
 app.use(express.json());
 
--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import {config} from 'dotenv';
 config();
 
 const pool = mysql.createPool({
-  host: process.env.HOST ,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  
 });
 
 
